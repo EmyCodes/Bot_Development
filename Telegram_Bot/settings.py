ca5b7707007756de90c2d89e7ed5eb6f874fcda7
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-06-07 14:15:39 +0100

    more call and response added

--- Telegram_Bot/settings.py
+++ Telegram_Bot/settings.py
@@ -1,1 +1,13 @@
-TELEGRAM_BOT_TOKEN=6135511650:AAFyBbETih1U7T8e0LicpxmbqDWUKTklCB0
+TELEGRAM_BOT_TOKEN = "727272727272727277:ishue78e78"
+
+CUSTOM_REPLY = {
+    "Hi": "Hello there",
+    "Hello": "Hi there!",
+    "Thank you": "You're mostly welcome!",
+    "Good Morning": "Good Morning",
+    "Good Afternoon": "Good Afternoon",
+    "Good Evening": "Good Evening",
+    "Goodnight": "Goodnight",
+    "How are you?": "I'm a Bot, I don't have feelings!! Okay!",
+    "Okay": "Yeah!"
+}
