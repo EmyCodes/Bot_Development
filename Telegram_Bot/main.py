92b7cd1db44f76c7fbfd99111173bf355aaa84ea
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-06-07 14:16:04 +0100

    MessageHandler used

--- Telegram_Bot/main.py
+++ Telegram_Bot/main.py
@@ -2,9 +2,10 @@
 
 import asyncio
 import logging
-from settings import TELEGRAM_BOT_TOKEN
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+from settings import TELEGRAM_BOT_TOKEN, CUSTOM_REPLY
 
 
 logging.basicConfig(
@@ -18,13 +19,19 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
     This function initializes a conversation with the bot.... 
     """
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    #Declaring firstname
+    first_name = update.message.from_user.first_name
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hello {first_name.split()[0]}\nI'm a bot, please talk to me!")
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
    """
     This function guides users on which commands to use....
    """
+    #Declaring firstnam
+    # first_name = update.message.from_user.first_name
+
    text = """
 /start    =>       Starts a Convesation with DeepChat
 /todolist =>       Tells DeepChat to get ready for TodoList Creation
@@ -42,8 +49,19 @@ async def todolist_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
         This function gets/fetch info from google calender
     
     """
+
+    #Declaring firstnam
+    # first_name = update.message.from_user.first_name
+
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, this is your to-do list")
-    pass
+
+async def custom_reply_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Custom replies function"""
+
+    message_text = update.effective_message.text
+    reply_text = CUSTOM_REPLY.get(message_text)
+    if reply_text:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
 
 
 if __name__ == "__main__":
@@ -55,9 +73,12 @@ if __name__ == "__main__":
     start_handler = CommandHandler('start', start_command)
     help_handler = CommandHandler('help', help_command)
     todolist_handler = CommandHandler('todolist', todolist_command)
+    custom_reply_handler = MessageHandler(filters.TEXT, custom_reply_command)
     application.add_handler(start_handler)
+    application.add_handler(custom_reply_handler)
     application.add_handler(help_handler)
     application.add_handler(todolist_handler)
 
     # Polling
     application.run_polling()
+
